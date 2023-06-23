8a379ba4477c2bf979c98e19ff589e735fe70b84
GusttavoCastilho <gustavocvalenciano@gmail.com>
2023-06-23 16:35:41 -0400

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Gustavo Castilho on 23/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
