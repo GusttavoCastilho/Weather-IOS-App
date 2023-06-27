23a23bea19f2a66ba33f1fedaad34f44f476449b
GusttavoCastilho <gustavocvalenciano@gmail.com>
2023-06-27 13:29:20 -0400

    feat: create WeatherWidget component

---
+++ Weather/Views/Components/WeatherWidget.swift
@@ -0,0 +1,59 @@
+//
+//  WeatherWidget.swift
+//  Weather
+//
+//  Created by Gustavo Castilho on 27/06/23.
+//
+
+import SwiftUI
+
+struct WeatherWidget: View {
+    var forecast: Forecast
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            Trapezoid()
+                .fill(Color.weatherWidgetBackground)
+                .frame(width: 342, height: 174)
+            
+            HStack(alignment: .bottom) {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("\(forecast.temperature)º")
+                        .font(.system(size: 64))
+                    
+                    VStack(alignment: .leading, spacing: 2) {
+                        Text("H:\(forecast.high)º  L:\(forecast.low)º")
+                            .font(.footnote)
+                            .foregroundColor(.secondary)
+                        
+                        Text(forecast.location)
+                            .font(.body)
+                            .lineLimit(1)
+                    }
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .trailing, spacing: 0) {
+                    Image("\(forecast.icon) large")
+                        .padding(.trailing, 4)
+                    
+                    Text(forecast.weather.rawValue)
+                        .font(.footnote)
+                        .padding(.trailing, 24)
+                }
+            }
+            .foregroundColor(.white)
+            .padding(.bottom, 20)
+            .padding(.leading, 20)
+        }
+        .frame(width: 342, height: 184, alignment: .bottom)
+    }
+}
+
+struct WeatherWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherWidget(forecast: Forecast.cities[0])
+            .preferredColorScheme(.dark)
+    }
+}
---
+++ Weather/Views/Detail/WeatherView.swift
@@ -0,0 +1,53 @@
+//
+//  WeatherView.swift
+//  Weather
+//
+//  Created by Gustavo Castilho on 27/06/23.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    @State private var searchText = ""
+    
+    var searchResults: [Forecast] {
+        if searchText.isEmpty {
+            return Forecast.cities
+        } else {
+            return Forecast.cities.filter { $0.location.contains(searchText) }
+        }
+    }
+    
+    var body: some View {
+        ZStack {
+            Color.background
+                .ignoresSafeArea()
+            
+            ScrollView(showsIndicators: false) {
+                VStack(spacing: 20) {
+                    ForEach(searchResults) { forecast in
+                        WeatherWidget(forecast: forecast)
+                    }
+                }
+            }
+            .safeAreaInset(edge: .top) {
+                EmptyView()
+                    .frame(height: 110)
+            }
+        }
+        .overlay {
+            NavigationBar(searchText: $searchText)
+        }
+        .navigationBarHidden(true)
+        .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search for a city or aiport")
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            WeatherView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
